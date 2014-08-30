dd789f02ec2cd5f6145bcfc62f6087ec46868d4e
lucywyman <wyman.lucy@gmail.com>
2014-08-30 10:59:23 -0700

    Ensuring that static directory gets included, attaching headers to top of slide, and adding osu tag to footer

--- README.rst
+++ README.rst
@@ -10,9 +10,10 @@ In conf.py::
     html_theme_path = osuosl_hieroglyph_theme.get_html_theme_path()
     html_static_path = ['_static', html_theme_path + '/osuosl/static/'] ##(must be changed in file)
     slide_theme_options = {'custom_css' : 'osuosl.css'}
-    slide_footer = "<img src='_static/logo.png'>"
+    slide_footer = "<img src='_static/osu-tag.png'><img src='_static/logo.png'>"
 
 For the html_theme and html_static_path variables, you must find the defaults in the file and change them, as they will not be overridden by your declaration of them! 
+
 ::
     
     make slides
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+#from distutils.core import setup
 from setuptools import setup
 
 with open('README.rst', 'r') as f:
@@ -11,12 +11,13 @@ setup(
             "hieroglyph>=0.6",
             ],
     name="osuosl_hieroglyph_theme",
-    version=0.3,
+    version=0.9,
     author="Lucy Wyman",
     author_email="lucyw@osuosl.org",
     description="Slides theme for  the OSU Open Source Lab",
     long_description=long_description,
     packages=['osuosl_hieroglyph_theme'],
+    package_data={'osuosl_hieroglyph_theme': ['osuosl_hieroglyph_theme/static']},
     include_package_data=True,
     keywords="hieroglyph extension theme",
     url="https://github.com/osuosl/osuosl_hieroglyph_theme",
